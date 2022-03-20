312e97c60f751682462c293e4f9eab6c9e9641a6
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-20 15:32:36 +0000

    Handle login submission and display error messages to improve ux

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from crypt import methods
+from flask import flash, redirect, render_template
 from app import app
 from app.forms import LoginForm
 
@@ -22,7 +23,10 @@ def index():
     ]
     return render_template('index.html', title='Home Page', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember me {form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
