ea3c09fa4b80c528e4b2765a29af3fe7f0f7eff8
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-20 21:20:41 +0000

    Finish login functionality to create session to users that sucessfully login

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from crypt import methods
 from flask import flash, redirect, render_template, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -25,8 +27,16 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember me {form.remember_me.data}')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid user or password')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
