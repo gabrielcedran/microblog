8d3fdb4ab10308dc34e7407fd00faba26635ea57
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-20 15:35:40 +0000

    Generate urls based on view functions rather than hardcoded text to reduce maintainability

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -28,5 +28,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember me {form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
