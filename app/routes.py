8a594d8f7b3fd15bb739ea6dc3f80fe1efc6fc14
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-20 13:20:49 +0000

    Control statements with templates (jinja2)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 'donbob'}
+    posts = [
+        {
+            'author': { 'username': 'Don'},
+            'body': 'My first post here, nice to meet you'
+        },
+        {
+            'author': { 'username': 'Bob'},
+            'body': 'I cannot wait for summer'
+        },
+        {
+            'author': { 'username': 'Mary'},
+            'body': 'Do you belive in E.T\'s?'
+        }
+    ]
+    return render_template('index.html', title='Home Page', user=user, posts=posts)
