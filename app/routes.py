3da258adac25b6ed17c45f807e54eb675e926720
Gabriel Cedran <gabrielcedran@gmail.com>
2022-03-20 21:24:24 +0000

    Implement logout functionality

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from crypt import methods
 from flask import flash, redirect, render_template, url_for
+from flask_login import logout_user
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user
@@ -40,3 +41,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
